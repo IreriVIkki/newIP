0e8ca939ca519b966c56175f19f32ee172d3ec1d
IreriVikki <wambsviki@gmail.com>
2018-09-04 06:25:01 +0300

    added a source route that will get news articles from only one source when clicked on

--- app/main/views.py
+++ app/main/views.py
@@ -26,3 +26,13 @@ def category(cat):
     # since these articles will be displayed on the same html as popular news. i name then exactly the same so that they can just overide popular news
     articles = get_categories(cat)
     return render_template('index.html', articles=articles, news_title=news_title)
+
+
+# defining a route that will get news from various sources when a source is clicked on
+@main.route('/sources/<source>')
+def sources(source):
+    # first check if a source actually exists
+    if source:
+        # call function that returnarticles from a gicen source
+        source_articles = get_source_articles(source)
+        return render_template('source.html', articles=source_articles)

06d61e4d08a4a6e2167078de63b44d645331b863
IreriVikki <wambsviki@gmail.com>
2018-09-04 06:50:01 +0300

    defined a function that will call the error html file when a user makes an error

--- app/main/errors.py
+++ app/main/errors.py
@@ -2,3 +2,9 @@ from flask import render_template
 
 # Importing the main application since it is the only application that will use this erreo handling class
 from . import main
+
+
+# defining a function that will route the user to an error html file when user searches for an item that does not exist
+@main.errorhandler(404)
+def four_Ow_four(error):
+    return render_template('fourOwfour.html'), 404
